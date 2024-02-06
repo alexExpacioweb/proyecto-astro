968d5216087145a2e7f4d7c59eaa316ca24d9027
Alex <alexmarin@expacioweb.com>
2024-02-06 18:30:57 +0100

    prueba componente react

---
+++ src/components/Entradas.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import getResults from '../utils/peticion2';
+
+const Entradas = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+
+        const getData = async () => {
+            const response = await getResults();
+            setData(response.data.posts.nodes);
+        }
+
+        getData();
+        
+    }, [])
+
+    console.log(data);
+  return (
+    <div>
+       {data.map((post) => {
+        return <div className="entrada" key={post.title}>
+                    <img src={post.featuredImage.node.mediaItemUrl} alt="entrada-image" />
+                    <div className="entrada-info">
+                        <h1>{post.title}</h1>
+                        <h1>post.content</h1>
+                    </div>
+                </div>
+    })}
+    </div>
+  )
+}
+
+export default Entradas
